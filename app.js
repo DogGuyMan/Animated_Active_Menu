1068c63a354a99306bd860c2763618378d60a71b
escatrgot <escatrgot@gmail.com>
2022-03-19 16:06:27 +0900

    ADD TS transition

---
+++ app.js
@@ -0,0 +1,24 @@
+//일단 LI의 위치를 가져온다
+//이벤트에 반응할수 있어야한다.
+// 어떻게 애니메이션 처리를 할것인가>
+var ulElement = document.querySelector("ul");
+var ulStyle = window.getComputedStyle(ulElement);
+var listElementArray = document.querySelectorAll("ul li.menu");
+var audioElement = document.querySelector("audio");
+var slideElement = document.querySelector("ul > li.slide");
+window.addEventListener('click', function () {
+    audioElement === null || audioElement === void 0 ? void 0 : audioElement.play();
+});
+if (slideElement != null) {
+    slideElement.style.width = "calc(100% / ".concat(listElementArray.length, ")");
+}
+function handleSlide(_E) {
+    if (slideElement != null && _E.target.tagName != "A") {
+        _E.stopPropagation();
+        slideElement.style.left = _E.target.offsetLeft + 'px';
+    }
+}
+//https://www.codegrepper.com/code-examples/javascript/frameworks/node_modules/add+event+listener+for+multiple+elements
+listElementArray.forEach(function (item) {
+    item.addEventListener("mouseenter", handleSlide);
+});
---
+++ app.ts
@@ -0,0 +1,28 @@
+//일단 LI의 위치를 가져온다
+//이벤트에 반응할수 있어야한다.
+// 어떻게 애니메이션 처리를 할것인가>
+
+const ulElement = document.querySelector("ul") as Element;
+const ulStyle = window.getComputedStyle(ulElement);
+const listElementArray = document.querySelectorAll<HTMLElement>("ul li.menu");
+const audioElement = document.querySelector("audio");
+const slideElement = document.querySelector<HTMLElement>("ul > li.slide");
+
+window.addEventListener('click', ()=>{
+    audioElement?.play();
+})
+
+if(slideElement != null){
+    slideElement.style.width = `calc(100% / ${listElementArray.length})`;
+}
+function handleSlide(_E : any){
+    if(slideElement != null && _E.target.tagName != "A"){
+        _E.stopPropagation()
+        slideElement.style.left = _E.target.offsetLeft + 'px';
+    }
+}
+
+//https://www.codegrepper.com/code-examples/javascript/frameworks/node_modules/add+event+listener+for+multiple+elements
+listElementArray.forEach(item => {
+    item.addEventListener("mouseenter" , handleSlide);
+})
